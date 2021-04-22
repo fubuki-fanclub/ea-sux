0c92e4d8998dc32798eff81bc32111435a81c8f4
Matic Babnik <matic.babnik@outlook.com>
2021-04-22 03:32:58 +0200

    Switched over to cdn.femboy.si

--- src/background/background.js
+++ src/background/background.js
@@ -10,7 +10,7 @@ chrome.storage.onChanged.addListener(settingsChanged)
 
 chrome.webRequest.onHeadersReceived.addListener(info => {
     return overridePlus ? {
-        redirectUrl: "https://raw.githubusercontent.com/fubuki-fanclub/ea-sux/master/student_app.js"
+        redirectUrl: "https://s3.eu-central-1.wasabisys.com/cdn.femboy.si/ea/student_app.js"
     } : {};
 }, {
     urls: [
